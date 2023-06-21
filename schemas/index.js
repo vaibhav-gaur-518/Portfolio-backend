df51c3ae10234dc7534238cc57bb97dc16e11e5d
vaibhav-gaur-518 <vaibhavgaur518@gmail.com>
2023-06-21 16:42:35 +0530

    backend

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import works from './works'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [works, abouts, skills, workExperience, experiences, contact]
---
+++ schemas/skills.js
@@ -0,0 +1,31 @@
+export default{
+    name:'skills',
+    title:'Skills',
+    type: 'document',
+    fields:[
+        {
+            name:'name',
+            title:'Name',
+            type:'string'
+        },
+        {
+            name:'bgColor',
+            title:'BgColor',
+            type:'string'
+        },
+        {
+            name:'tag',
+            title:'Tag',
+            type:'string'
+        },
+        {
+            name:'icon',
+            title:'Icon',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },
+        
+    ]
+}
